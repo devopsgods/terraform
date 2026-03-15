28c25bddcf6643600a76b3b701421fed4f25e0fb
Devils <devopsdevil9gmail.com>
2026-03-15 16:04:47 +0000

    attempt 2

---
+++ count/ec2.tf
@@ -0,0 +1,34 @@
+resource "aws_instance" "this" {
+    count = 10
+  ami           = "ami-0220d79f3f480ecf5"
+  instance_type = "t3.micro"
+  vpc_security_group_ids = [aws_security_group.allow_tls.id]
+
+  tags = {
+    Name = var.instance[count.index]
+    project = "Roboshop"
+  }
+}
+
+resource "aws_security_group" "allow_tls" {
+    name        = var.sg_name
+    description = var.sg_description
+    
+    egress {
+    from_port        = var.sg_from_port
+    to_port          = var.sg_to_port
+    protocol         = "-1"
+    cidr_blocks      = var.cidr_blocks
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  ingress {
+    from_port        = var.sg_from_port
+    to_port          = var.sg_to_port
+    protocol         = "-1"
+    cidr_blocks      = var.cidr_blocks
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = var.sg_tags
+}
